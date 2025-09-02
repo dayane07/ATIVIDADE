77cc27c36c528e0fd3c32f406236d054b6abf42d
dayane07 <dayaanez9008@gmail.com>
2025-09-02 18:32:09 -0300

    #calculadora simples

---
+++ exercicio02.py
@@ -0,0 +1,13 @@
+num1 = int(input("digite um numero: "))
+num2 = int(input("digite o segundo numero: "))
+
+soma = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+div = num1 % num2
+
+print ("a soma de numero1 e numero2 é:", soma)
+print ("a subtração de numero1 e numero2 é:", sub)
+print ("a multiplicão de numero1 e numero2 é:", multi)
+print ("a divisão de numero1 e numero2 é:", div)
+
