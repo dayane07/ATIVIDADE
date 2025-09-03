4c9a536f6bc036dbfacfd2a5a40e2e289e1811b2
dayane07 <dayaanez9008@gmail.com>
2025-09-02 23:51:58 -0300

    #conversor de moedas

--- exercicio04.py
+++ exercicio04.py
@@ -2,4 +2,4 @@ altura = float(input("informe a altura em metros:"))
 largura = float(input("informe a largura em metros:"))
 area = altura * largura
 
-print("um retângulo com a altura de",altura,"metros (m) e largura de",largura,"metros (m) tem a área de",area,"metros quadrados (m²).")
+print("um retângulo com a altura de",altura,"metros e largura de",largura,"metros tem a área de",area,"metros quadrados (m²).")
---
+++ exercicio05.py
@@ -0,0 +1,5 @@
+dólar = 5.25
+conversão = float(input("Insira o valor em reais (R$):"))
+resultado = conversão * dólar
+
+print("o valor R$", conversão, "equivale a US$", resultado)
